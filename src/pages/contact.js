01152e88ff9f44214fa896c9438a6d0a4da2999b
flex6699 <54765869+flex6699@users.noreply.github.com>
2024-02-15 01:04:30 +0530

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,8 +1,15 @@
 import React from "react";
-
+import { useEffect } from 'react';
 import ContactSection from "../components/ContactSection";
 import { motion } from "framer-motion/dist/framer-motion";
 export default function Contact() {
+   useEffect(() => {
+    // Set the title when the component mounts
+    document.title = "Contact";
+
+    // Optionally, you can return a cleanup function to revert the title when the component unmounts
+    
+  }, []);
   return (
     <motion.div
       inital={{ width: 0 }}
